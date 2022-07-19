56099e9f130c2294fed31ac747201d2c2ce812e8
Miles Granger <miles59923@gmail.com>
2022-07-19 05:01:23 +0200

    Updates to compressor (#3)

--- isal-rs/src/igzip.rs
+++ isal-rs/src/igzip.rs
@@ -6,7 +6,7 @@ use std::os::raw::c_int;
 use isal_sys as isal;
 
 /// Buffer size
-pub const BUF_SIZE: usize = 8192;
+pub const BUF_SIZE: usize = 16 * 1024;
 
 /// Result type
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
@@ -21,9 +21,10 @@ pub mod read {
         level: CompressionLevel,
         in_buf: [u8; BUF_SIZE],
         out_buf: [u8; BUF_SIZE],
+        dsts: usize,
+        dste: usize,
         level_buf: Vec<u8>,
         is_gzip: bool, // gzip or deflate
-        n_bytes_encoded: usize,
     }
 
     impl<R: io::Read> Encoder<R> {
@@ -34,9 +35,9 @@ pub mod read {
             let out_buf = [0_u8; BUF_SIZE as _];
 
             zstream.end_of_stream = 0;
-            zstream.flush = isal::NO_FLUSH as _;
+            zstream.flush = isal::SYNC_FLUSH as _;
 
-            zstream.level = level as _;
+            zstream.level = 3; //level as _;
             zstream.gzip_flag = is_gzip as _;
 
             let level_buf_size = isal::ISAL_DEF_LVL3_LARGE; // TODO: set level buf sizes
@@ -52,44 +53,55 @@ pub mod read {
                 in_buf,
                 out_buf,
                 is_gzip,
-                n_bytes_encoded: 0,
+                dste: 0,
+                dsts: 0,
             }
         }
 
         pub fn get_ref(&mut self) -> &mut R {
             &mut self.inner
         }
+
+        pub fn read_from_out_buf(&mut self, buf: &mut [u8]) -> usize {
+            let available_bytes = self.dste - self.dsts;
+            let count = std::cmp::min(available_bytes, buf.len());
+            buf[..count].copy_from_slice(&self.out_buf[self.dsts..self.dsts + count]);
+            self.dsts += count;
+            count
+        }
     }
 
     impl<R: io::Read> io::Read for Encoder<R> {
-        fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
-            let mut n_bytes = 0;
-
-            // TODO: impl level one condition: https://github.com/intel/isa-l/blob/62519d97ec8242dce393a1f81593f4f67da3ac92/igzip/igzip_example.c#L70
-            while self.stream.internal_state.state != isal::isal_zstate_state_ZSTATE_END {
+        fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            let count = self.read_from_out_buf(buf);
+            if count > 0 {
+                Ok(count)
+            } else if self.stream.internal_state.state != isal::isal_zstate_state_ZSTATE_END {
                 // read input into buffer
                 self.stream.avail_in = self.inner.read(&mut self.in_buf).map(|v| v as u32)?;
                 self.stream.end_of_stream =
                     (self.stream.avail_in < self.in_buf.len() as u32) as u16;
+
+                if self.stream.end_of_stream == 1 {
+                    self.stream.flush = isal::FULL_FLUSH as _;
+                }
                 self.stream.next_in = self.in_buf.as_mut_ptr();
 
                 // compress this block
-                loop {
-                    self.stream.avail_out = BUF_SIZE as _;
-                    self.stream.next_out = self.out_buf.as_mut_ptr();
-                    unsafe { isal::isal_deflate(&mut self.stream as *mut _) };
-                    n_bytes += buf.write(
-                        &mut self.out_buf[..BUF_SIZE as usize - self.stream.avail_out as usize],
-                    )?;
-                    if self.stream.avail_out == BUF_SIZE as _ {
-                        break;
-                    }
-                }
+                self.stream.avail_out = self.out_buf.len() as _;
+                self.stream.next_out = self.out_buf.as_mut_ptr();
 
-                assert!(self.stream.avail_in == 0);
+                debug_assert_eq!(unsafe { isal::isal_deflate(&mut self.stream) }, 0);
+
+                self.dsts = 0;
+                self.dste = self.stream.avail_out as usize;
+
+                Ok(self.read_from_out_buf(buf))
+            } else {
+                Ok(0)
             }
 
-            Ok(n_bytes)
+            // TODO: impl level one condition: https://github.com/intel/isa-l/blob/62519d97ec8242dce393a1f81593f4f67da3ac92/igzip/igzip_example.c#L70
         }
     }
 
@@ -116,7 +128,7 @@ pub(crate) fn new_zstream(
 }
 
 /// Compress `input` directly into `output`. This is the fastest possible compression available.
-#[inline]
+#[inline(always)]
 pub fn compress_into(input: &[u8], output: &mut [u8], level: u8, is_gzip: bool) -> Result<usize> {
     let mut zstream = new_zstream(isal::isal_deflate_stateless_init);
 
@@ -142,27 +154,45 @@ pub fn compress_into(input: &[u8], output: &mut [u8], level: u8, is_gzip: bool) 
 }
 
 /// Compress `input`
-#[inline]
-pub fn compress<R: Read>(input: R, level: CompressionLevel, is_gzip: bool) -> Result<Vec<u8>> {
-    let mut encoder = read::Encoder::new(input, level, is_gzip);
-    let mut output = vec![0; BUF_SIZE];
+#[inline(always)]
+pub fn compress(input: &[u8], level: CompressionLevel, is_gzip: bool) -> Result<Vec<u8>> {
+    let mut buf = vec![];
 
-    // TODO: maybe more efficient way to extend buffer?
     let mut n_bytes = 0;
-    loop {
-        let n_read = encoder.read(&mut output[n_bytes..])?;
-        n_bytes += n_read;
 
-        // Return completed output or resize (if needed) and continue.
-        if n_read == 0 {
-            output.truncate(n_bytes);
-            break Ok(output);
+    let mut zstream = new_zstream(isal::isal_deflate_init);
 
-        // Resize to at least BUF_SIZE but not more than twice BUF_SIZE
-        } else if output.len() - n_bytes < BUF_SIZE {
-            output.resize(output.len() + BUF_SIZE, 0);
-        }
+    zstream.end_of_stream = 1;
+    zstream.flush = isal::NO_FLUSH as _;
+
+    zstream.level = 3; //level as _;
+    zstream.gzip_flag = is_gzip as _;
+
+    let level_buf_size = isal::ISAL_DEF_LVL3_DEFAULT; // TODO: set level buf sizes
+    let mut level_buf = vec![0_u8; level_buf_size as _];
+    zstream.level_buf = level_buf.as_mut_ptr();
+    zstream.level_buf_size = level_buf.len() as _;
+
+    // TODO: impl level one condition: https://github.com/intel/isa-l/blob/62519d97ec8242dce393a1f81593f4f67da3ac92/igzip/igzip_example.c#L70
+    // read input into buffer
+    zstream.avail_in = input.len() as _;
+    zstream.next_in = input.as_ptr() as *mut _;
+
+    // compress this block
+    //dbg!(zstream.avail_in, zstream.internal_state.state, zstream.end_of_stream);
+    //std::thread::sleep(std::time::Duration::from_millis(50));
+    while zstream.internal_state.state != isal::isal_zstate_state_ZSTATE_END {
+        buf.resize(buf.len() + BUF_SIZE, 0);
+        zstream.avail_out = BUF_SIZE as _;
+        zstream.next_out = buf[n_bytes..n_bytes + BUF_SIZE].as_mut_ptr();
+
+        unsafe { isal::isal_deflate(&mut zstream as *mut _) };
+
+        let n = BUF_SIZE - zstream.avail_out as usize;
+        n_bytes += n;
     }
+    buf.truncate(n_bytes);
+    Ok(buf)
 }
 
 /// Combine error handling for both isal_deflate/_stateless functions
@@ -202,23 +232,38 @@ enum MemLevel {
 #[cfg(test)]
 mod tests {
 
-    use std::io::Cursor;
+    use std::{fs, io::Cursor};
 
     use super::*;
 
+    fn get_data() -> Vec<u8> {
+        fs::read(format!(
+            "{}/../../pyrus-cramjam/benchmarks/data/html_x_4",
+            env!("CARGO_MANIFEST_DIR")
+        ))
+        .unwrap()
+    }
+
     #[test]
     fn basic_compress_into() {
-        let data = b"bytes here";
-        let mut output = vec![0_u8; 50];
+        let data = get_data();
+        let mut output = vec![0_u8; data.len()]; // assume compression isn't worse than input len.
         let n_bytes = compress_into(data.as_slice(), &mut output, 3, true).unwrap();
-        println!("n_bytes: {} - {:?}", n_bytes, &output[..n_bytes]);
+        println!(
+            "n_bytes: {} - {:?}",
+            n_bytes,
+            &output[..std::cmp::min(output.len() - 1, 100)]
+        );
     }
 
     #[test]
     fn basic_compress() {
-        let data = b"bytes here";
-        let rdr = Cursor::new(data);
-        let output = compress(rdr, CompressionLevel::Three, true).unwrap();
-        println!("n_bytes: {:?}", &output);
+        let data = get_data();
+        let output = compress(&data, CompressionLevel::Three, true).unwrap();
+        dbg!(output.len());
+        println!(
+            "n_bytes: {:?}",
+            &output[..std::cmp::min(output.len() - 1, 100)]
+        );
     }
 }
