ebd59b9ef232dd11837c020248d479aa22058d87
Miles Granger <miles59923@gmail.com>
2022-07-22 05:57:54 +0200

    Add compress example used for perf

---
+++ isal-rs/examples/compress.rs
@@ -0,0 +1,18 @@
+use std::fs;
+
+use isal_rs::igzip;
+
+fn get_data() -> std::result::Result<Vec<u8>, std::io::Error> {
+    fs::read(format!(
+        "{}/../../pyrus-cramjam/benchmarks/data/html_x_4",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+}
+
+
+fn main() {
+    let data = get_data().unwrap();    
+    for _ in 0..1000 {
+        let _v = igzip::compress(&data, igzip::CompressionLevel::Three, true).unwrap();
+    }
+}
