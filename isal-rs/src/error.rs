952f775ae97e05e3ef32cadd5b630e59287e78f0
Miles Granger <miles59923@gmail.com>
2022-07-24 06:38:27 +0200

    Add error file

---
+++ isal-rs/src/error.rs
@@ -0,0 +1,51 @@
+use std::{fmt, io, result};
+
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+
+    // isal compression errors
+    InvalidFlush,
+    InvalidLevel,
+    InvalidLevelBuf,
+    StatelessOverflow,
+
+    // isal decompression errors
+    InvalidBlock,
+    NeedDict,
+    InvalidSymbol,
+    InvalidLookBack,
+    InvalidWrapper,
+    UnsupportedMethod,
+    IncorrectChecksum,
+
+    // Anything else not covered, exit code, message
+    Other((Option<isize>, String)),
+}
+
+impl From<Error> for io::Error {
+    fn from(err: Error) -> io::Error {
+        io::Error::new(io::ErrorKind::Other, err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // write!(f, msg)
+        match self {
+            Error::Io(err) => write!(f, "{err}"),
+            Error::Other((exit_code, msg)) => write!(f, "{msg} (Exit code: {exit_code:?})"),
+            _ => write!(f, "{self:?}"),
+        }
+    }
+}
