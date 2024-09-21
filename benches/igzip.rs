c332620103945a05f584a9c4ddfac009142cb52e
Miles Granger <miles59923@gmail.com>
2024-09-21 06:29:42 +0200

    Fix benches

--- benches/igzip.rs
+++ benches/igzip.rs
@@ -1,23 +1,28 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use isal_rs::igzip::{self, read::Encoder};
-use std::{fs, io};
+use isal::igzip::{self, read::Encoder};
+use std::io;
+use std::io::Cursor;
 
-fn get_data() -> std::result::Result<Vec<u8>, std::io::Error> {
-    fs::read(format!("{}/test-data/html_x_4", env!("CARGO_MANIFEST_DIR")))
+fn get_data() -> Vec<u8> {
+    (0..10_000)
+        .map(|_| b"oh what a beautiful morning, oh what a beautiful day!!".to_vec())
+        .flat_map(|v| v)
+        .collect()
 }
 
 fn igzip_compress(c: &mut Criterion) {
-    let data = get_data().unwrap();
+    let data = get_data();
     c.bench_function("igzip::compress", |b| {
         b.iter(|| {
-            let _ = igzip::compress(&data, igzip::CompressionLevel::Three, true).unwrap();
+            let _ =
+                igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
         })
     });
 }
 
 fn igzip_compress_into(c: &mut Criterion) {
-    let data = get_data().unwrap();
-    let compressed_len = igzip::compress(&data, igzip::CompressionLevel::Three, true)
+    let data = get_data();
+    let compressed_len = igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true)
         .unwrap()
         .len();
     let mut compressed = vec![0; compressed_len];
@@ -31,19 +36,21 @@ fn igzip_compress_into(c: &mut Criterion) {
 }
 
 fn igzip_decompress(c: &mut Criterion) {
-    let data = get_data().unwrap();
-    let compressed = igzip::compress(&data, igzip::CompressionLevel::Three, true).unwrap();
+    let data = get_data();
+    let compressed =
+        igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
     c.bench_function("igzip::decompress", |b| {
         b.iter(|| {
-            let _ = igzip::decompress(&compressed).unwrap();
+            let _ = igzip::decompress(Cursor::new(&compressed)).unwrap();
         })
     });
 }
 
 fn igzip_decompress_into(c: &mut Criterion) {
-    let data = get_data().unwrap();
-    let compressed = igzip::compress(&data, igzip::CompressionLevel::Three, true).unwrap();
-    let mut decompressed = vec![0; data.len()];
+    let data = get_data();
+    let compressed =
+        igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
+    let mut decompressed = vec![0; data.len() + 10000];
     c.bench_function("igzip::decompress_into", |b| {
         b.iter(|| {
             let _ = igzip::decompress_into(&compressed, &mut decompressed).unwrap();
@@ -52,18 +59,19 @@ fn igzip_decompress_into(c: &mut Criterion) {
 }
 
 fn igzip_roundtrip(c: &mut Criterion) {
-    let data = get_data().unwrap();
+    let data = get_data();
     c.bench_function("igzip::roundtrip", |b| {
         b.iter(|| {
-            let compressed = igzip::compress(&data, igzip::CompressionLevel::Three, true).unwrap();
-            let _ = igzip::decompress(&compressed).unwrap();
+            let compressed =
+                igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
+            let _ = igzip::decompress(Cursor::new(&compressed)).unwrap();
         })
     });
 }
 
 fn igzip_roundtrip_into(c: &mut Criterion) {
-    let data = get_data().unwrap();
-    let compressed_len = igzip::compress(&data, igzip::CompressionLevel::Three, true)
+    let data = get_data();
+    let compressed_len = igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true)
         .unwrap()
         .len();
 
@@ -81,7 +89,7 @@ fn igzip_roundtrip_into(c: &mut Criterion) {
 }
 
 fn igzip_encoder(c: &mut Criterion) {
-    let data = get_data().unwrap();
+    let data = get_data();
     c.bench_function("igzip::read::Encoder", |b| {
         b.iter(|| {
             let mut output = vec![];
@@ -93,7 +101,7 @@ fn igzip_encoder(c: &mut Criterion) {
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().sample_size(1_000);
+    config = Criterion::default().sample_size(250);
     targets =
         igzip_compress,
         igzip_compress_into,
