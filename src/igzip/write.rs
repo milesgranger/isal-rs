744ef1938d5f4669305861add38fc0fe007f4428
Miles Granger <miles59923@gmail.com>
2024-09-23 13:17:53 +0200

    impl write::Decoder

--- src/igzip/write.rs
+++ src/igzip/write.rs
@@ -155,6 +155,128 @@ impl<W: io::Write> io::Write for Encoder<W> {
     }
 }
 
+/// Streaming compression for input streams implementing `std::io::Read`.
+///
+/// Notes
+/// -----
+/// One should consider using `crate::igzip::decompress` or `crate::igzip::decompress_into` if possible.
+/// In that context, we do not need to hold and maintain intermediate buffers for reading and writing.
+///
+/// Example
+/// -------
+/// ```
+/// use std::{io, io::Write};
+/// use isal::igzip::{write::Decoder, CompressionLevel, compress};
+/// let data = b"Hello, World!".to_vec();
+///
+/// let compressed = compress(io::Cursor::new(data.as_slice()), CompressionLevel::Three, true).unwrap();
+///
+/// let mut decompressed = vec![];
+/// let mut decoder = Decoder::new(&mut decompressed);
+///
+/// // Numbeer of compressed bytes written to `output`
+/// let n = io::copy(&mut io::Cursor::new(&compressed), &mut decoder).unwrap();
+/// assert_eq!(n as usize, compressed.len());
+/// assert_eq!(decompressed.as_slice(), data);
+/// ```
+pub struct Decoder<W: io::Write> {
+    inner: W,
+    zst: InflateState,
+    out_buf: Vec<u8>,
+    dsts: usize,
+    dste: usize,
+}
+
+impl<W: io::Write> Decoder<W> {
+    pub fn new(writer: W) -> Decoder<W> {
+        let mut zst = InflateState::new();
+        zst.0.crc_flag = isal::IGZIP_GZIP;
+
+        Self {
+            inner: writer,
+            zst,
+            out_buf: Vec::with_capacity(BUF_SIZE),
+            dste: 0,
+            dsts: 0,
+        }
+    }
+
+    /// Mutable reference to underlying reader, not advisable to modify during reading.
+    pub fn get_ref_mut(&mut self) -> &mut W {
+        &mut self.inner
+    }
+
+    // Reference to underlying reader
+    pub fn get_ref(&self) -> &W {
+        &self.inner
+    }
+
+    #[inline(always)]
+    fn write_from_out_buf(&mut self) -> io::Result<usize> {
+        let count = self.dste - self.dsts;
+        self.inner
+            .write_all(&mut self.out_buf[self.dsts..self.dste])?;
+        self.out_buf.truncate(0);
+        self.dsts = 0;
+        self.dste = 0;
+        Ok(count)
+    }
+}
+
+impl<W: io::Write> io::Write for Decoder<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        // Check if there is data left in out_buf, otherwise refill; if end state, return 0
+        // Read out next buf len worth to compress; filling intermediate out_buf
+        self.zst.0.avail_in = buf.len() as _;
+        self.zst.0.next_in = buf.as_ptr() as *mut _;
+
+        let mut n_bytes = 0;
+        while self.zst.0.avail_in > 0 {
+            if self.zst.block_state() == isal::isal_block_state_ISAL_BLOCK_NEW_HDR {
+                // Read this member's gzip header
+                let mut gz_hdr: mem::MaybeUninit<isal::isal_gzip_header> =
+                    mem::MaybeUninit::uninit();
+                unsafe { isal::isal_gzip_header_init(gz_hdr.as_mut_ptr()) };
+                let mut gz_hdr = unsafe { gz_hdr.assume_init() };
+                read_gzip_header(&mut self.zst.0, &mut gz_hdr)?;
+            }
+
+            // decompress member
+            loop {
+                self.out_buf.resize(n_bytes + BUF_SIZE, 0);
+
+                self.zst.0.next_out = self.out_buf[n_bytes..n_bytes + BUF_SIZE].as_mut_ptr();
+                self.zst.0.avail_out = BUF_SIZE as _;
+
+                self.zst.step_inflate()?;
+
+                n_bytes += BUF_SIZE - self.zst.0.avail_out as usize;
+
+                let state = self.zst.block_state();
+                if state == isal::isal_block_state_ISAL_BLOCK_CODED
+                    || state == isal::isal_block_state_ISAL_BLOCK_TYPE0
+                    || state == isal::isal_block_state_ISAL_BLOCK_HDR
+                    || state == isal::isal_block_state_ISAL_BLOCK_FINISH
+                {
+                    break;
+                }
+            }
+            if self.zst.0.block_state == isal::isal_block_state_ISAL_BLOCK_FINISH {
+                self.zst.reset();
+            }
+        }
+        self.out_buf.truncate(n_bytes);
+        self.dste = n_bytes;
+        self.dsts = 0;
+        self.write_from_out_buf()?;
+
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
@@ -211,6 +333,40 @@ pub mod tests {
         assert_eq!(encoder.total_in(), first.len() + second.len());
 
         let decompressed = crate::igzip::decompress(io::Cursor::new(&compressed)).unwrap();
+        assert_eq!(&decompressed, b"foobar");
+    }
+
+    #[test]
+    fn test_decoder_basic() {
+        let data = gen_large_data();
+
+        let compressed =
+            crate::igzip::compress(io::Cursor::new(&data), CompressionLevel::Three, true).unwrap();
+
+        let mut decompressed = vec![];
+        let mut decoder = Decoder::new(&mut decompressed);
+        let nbytes = io::copy(&mut io::Cursor::new(&compressed), &mut decoder).unwrap();
+        assert_eq!(nbytes, compressed.len() as u64);
+        assert_eq!(decompressed.len(), data.len());
+    }
+
+    #[test]
+    fn test_decoder_multi_stream() {
+        let first = b"foo";
+        let second = b"bar";
+
+        let mut compressed =
+            crate::igzip::compress(io::Cursor::new(&first), CompressionLevel::Three, true).unwrap();
+        compressed.extend(
+            crate::igzip::compress(io::Cursor::new(&second), CompressionLevel::Three, true)
+                .unwrap(),
+        );
+
+        let mut decompressed = vec![];
+        let mut decoder = Decoder::new(&mut decompressed);
+
+        let nbytes = io::copy(&mut io::Cursor::new(&compressed), &mut decoder).unwrap();
+        assert_eq!(nbytes, compressed.len() as _);
         assert_eq!(&decompressed, b"foobar");
     }
 }
