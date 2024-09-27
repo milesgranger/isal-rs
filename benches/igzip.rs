940aa6a4c1736fc02f110d0b93faf758752e4b29
Miles Granger <miles59923@gmail.com>
2024-09-27 04:51:43 +0200

    Make semi-random data

--- benches/igzip.rs
+++ benches/igzip.rs
@@ -1,100 +1,89 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use isal::igzip::{self, read::Encoder};
-use std::io;
-use std::io::Cursor;
+use isal::igzip;
+use rand::Rng;
+use std::io::{self, Cursor};
+
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+// Generate some semi-random data
+fn generate_random_sentence() -> Vec<u8> {
+    let words = [
+        "the",
+        "quick",
+        "brown",
+        "fox",
+        "jumps",
+        "over",
+        "the",
+        "lazy",
+        "dog",
+        "hello",
+        "world",
+        "data",
+        "compression",
+        "test",
+        "rust",
+        "benchmark",
+    ];
+    let mut rng = thread_rng();
+    // sentences 5 - 15 words long
+    let sentence_length = 5 + (rng.gen_range(0..10));
+    let sentence: Vec<&str> = (0..sentence_length)
+        .map(|_| *words.choose(&mut rng).unwrap())
+        .collect();
+    (sentence.join(" ") + ".").as_bytes().to_vec()
+}
 
 fn get_data() -> Vec<u8> {
-    (0..500_000)
-        .map(|_| b"oh what a beautiful morning, oh what a beautiful day!!".to_vec())
-        .flat_map(|v| v)
-        .collect()
+    (0..10_000)
+        .map(|_| generate_random_sentence())
+        .flatten()
+        .collect::<Vec<u8>>()
 }
 
-fn igzip_compress(c: &mut Criterion) {
+fn gzip_read_encoder(c: &mut Criterion) {
     let data = get_data();
-    c.bench_function("igzip::compress", |b| {
-        b.iter(|| {
-            let _ =
-                igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
-        })
-    });
-}
-
-fn igzip_compress_into(c: &mut Criterion) {
-    let data = get_data();
-    let compressed_len = igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true)
-        .unwrap()
-        .len();
-    let mut compressed = vec![0; compressed_len];
-    c.bench_function("igzip::compress_into", |b| {
-        b.iter(|| {
-            let _ =
-                igzip::compress_into(&data, &mut compressed, igzip::CompressionLevel::Three, true)
-                    .unwrap();
-        })
-    });
-}
-
-fn igzip_decompress(c: &mut Criterion) {
-    let data = get_data();
-    let compressed =
-        igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
-    c.bench_function("igzip::decompress", |b| {
-        b.iter(|| {
-            let _ = igzip::decompress(Cursor::new(&compressed)).unwrap();
-        })
-    });
-}
-
-fn igzip_decompress_into(c: &mut Criterion) {
-    let data = get_data();
-    let compressed =
-        igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
-    let mut decompressed = vec![0; data.len() + 10000];
-    c.bench_function("igzip::decompress_into", |b| {
-        b.iter(|| {
-            let _ = igzip::decompress_into(&compressed, &mut decompressed).unwrap();
-        })
-    });
-}
-
-fn igzip_roundtrip(c: &mut Criterion) {
-    let data = get_data();
-    c.bench_function("igzip::roundtrip", |b| {
-        b.iter(|| {
-            let compressed =
-                igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true).unwrap();
-            let _ = igzip::decompress(Cursor::new(&compressed)).unwrap();
-        })
-    });
-}
-
-fn igzip_roundtrip_into(c: &mut Criterion) {
-    let data = get_data();
-    let compressed_len = igzip::compress(Cursor::new(&data), igzip::CompressionLevel::Three, true)
-        .unwrap()
-        .len();
-
-    let mut decompressed = vec![0; data.len()];
-    let mut compressed = vec![0; compressed_len];
-
-    c.bench_function("igzip::roundtrip_into", |b| {
-        b.iter(|| {
-            let _ =
-                igzip::compress_into(&data, &mut compressed, igzip::CompressionLevel::Three, true)
-                    .unwrap();
-            let _ = igzip::decompress_into(&compressed, &mut decompressed).unwrap();
-        })
-    });
-}
-
-fn igzip_encoder(c: &mut Criterion) {
-    let data = get_data();
-    c.bench_function("igzip::read::Encoder", |b| {
+    c.bench_function("igzip::read::GzipEncoder", |b| {
         b.iter(|| {
             let mut output = vec![];
-            let mut encoder = Encoder::new(data.as_slice(), igzip::CompressionLevel::Three, true);
+            let mut encoder =
+                igzip::read::GzipEncoder::new(data.as_slice(), igzip::CompressionLevel::Three);
             let _ = io::copy(&mut encoder, &mut output).unwrap();
+        })
+    });
+}
+fn flate2_gzip_read_encoder(c: &mut Criterion) {
+    let data = get_data();
+    c.bench_function("flate2::read::GzEncoder", |b| {
+        b.iter(|| {
+            let mut output = vec![];
+            let mut encoder =
+                flate2::read::GzEncoder::new(data.as_slice(), flate2::Compression::best());
+            let _ = io::copy(&mut encoder, &mut output).unwrap();
+        })
+    });
+}
+
+fn gzip_write_encoder(c: &mut Criterion) {
+    let data = get_data();
+    c.bench_function("igzip::write::GzipEncoder", |b| {
+        b.iter(|| {
+            let mut output = vec![];
+            let mut encoder =
+                igzip::write::GzipEncoder::new(&mut output, igzip::CompressionLevel::Three);
+            let _ = io::copy(&mut Cursor::new(&data), &mut encoder).unwrap();
+        })
+    });
+}
+fn flate2_gzip_write_encoder(c: &mut Criterion) {
+    let data = get_data();
+    c.bench_function("flate2::write::GzEncoder", |b| {
+        b.iter(|| {
+            let mut output = vec![];
+            let mut encoder =
+                flate2::write::GzEncoder::new(&mut output, flate2::Compression::best());
+            let _ = io::copy(&mut Cursor::new(&data), &mut encoder).unwrap();
         })
     });
 }
@@ -103,12 +92,9 @@ criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(50);
     targets =
-        igzip_compress,
-        igzip_compress_into,
-        igzip_decompress,
-        igzip_decompress_into,
-        igzip_roundtrip,
-        igzip_roundtrip_into,
-        igzip_encoder
+        gzip_read_encoder,
+        flate2_gzip_read_encoder,
+        gzip_write_encoder,
+        flate2_gzip_write_encoder
 }
 criterion_main!(benches);
