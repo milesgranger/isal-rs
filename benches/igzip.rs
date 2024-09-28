6341a6e4f13a4b04d7467a744c3ae8be4751e8e8
Miles Granger <miles59923@gmail.com>
2024-09-28 14:36:32 +0200

    Decent enough benchmarks

--- benches/igzip.rs
+++ benches/igzip.rs
@@ -1,44 +1,50 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use isal::igzip;
-use std::io::{self, Cursor};
+use std::{
+    collections::HashMap,
+    io::{self, Cursor},
+};
 
-static DATA: [&'static [u8]; 25] = [
-    include_bytes!("data/COPYING"),
-    include_bytes!("data/alice29.txt"),
-    include_bytes!("data/asyoulik.txt"),
-    include_bytes!("data/fireworks.jpeg"),
-    include_bytes!("data/geo.protodata"),
-    include_bytes!("data/html"),
-    include_bytes!("data/html_x_4"),
-    include_bytes!("data/kppkn.gtb"),
-    include_bytes!("data/lcet10.txt"),
-    include_bytes!("data/Mark.Twain-Tom.Sawyer.txt"),
-    include_bytes!("data/mr"),
-    include_bytes!("data/ooffice"),
-    include_bytes!("data/osdb"),
-    include_bytes!("data/paper-100k.pdf"),
-    include_bytes!("data/plrabn12.txt"),
-    include_bytes!("data/sao"),
-    include_bytes!("data/urls.10K"),
-    include_bytes!("data/xml"),
-    include_bytes!("data/x-ray"),
-    include_bytes!("data/reymont"),
-    include_bytes!("data/dickens"),
-    include_bytes!("data/samba"),
-    include_bytes!("data/nci"),
-    include_bytes!("data/mozilla"),
-    include_bytes!("data/webster"),
+static DATA: [(&'static str, &'static [u8]); 25] = [
+    ("COPYING", include_bytes!("data/COPYING")),
+    ("alice29.txt", include_bytes!("data/alice29.txt")),
+    ("asyoulik.txt", include_bytes!("data/asyoulik.txt")),
+    ("fireworks.jpeg", include_bytes!("data/fireworks.jpeg")),
+    ("geo.protodata", include_bytes!("data/geo.protodata")),
+    ("html", include_bytes!("data/html")),
+    ("html_x_4", include_bytes!("data/html_x_4")),
+    ("kppkn.gtb", include_bytes!("data/kppkn.gtb")),
+    ("lcet10.txt", include_bytes!("data/lcet10.txt")),
+    (
+        "Mark.Twain-Tom.Sawyer.txt",
+        include_bytes!("data/Mark.Twain-Tom.Sawyer.txt"),
+    ),
+    ("mr", include_bytes!("data/mr")),
+    ("ooffice", include_bytes!("data/ooffice")),
+    ("osdb", include_bytes!("data/osdb")),
+    ("paper-100k.pdf", include_bytes!("data/paper-100k.pdf")),
+    ("plrabn12.txt", include_bytes!("data/plrabn12.txt")),
+    ("sao", include_bytes!("data/sao")),
+    ("urls.10K", include_bytes!("data/urls.10K")),
+    ("xml", include_bytes!("data/xml")),
+    ("x-ray", include_bytes!("data/x-ray")),
+    ("reymont", include_bytes!("data/reymont")),
+    ("dickens", include_bytes!("data/dickens")),
+    ("samba", include_bytes!("data/samba")),
+    ("nci", include_bytes!("data/nci")),
+    ("mozilla", include_bytes!("data/mozilla")),
+    ("webster", include_bytes!("data/webster")),
 ];
 
 // Get a subset of the data, using all of it takes a very long time
 // so can mix-match when in doubt or debugging. As of now, it selects
 // a decent mix I think, last one being a larger file 'webster' @ ~40M
-fn get_data() -> [&'static [u8]; 6] {
-    [DATA[1], DATA[5], DATA[10], DATA[15], DATA[20], DATA[24]]
+fn get_data() -> HashMap<&'static str, &'static [u8]> {
+    HashMap::from_iter([DATA[1], DATA[5], DATA[10], DATA[15], DATA[20], DATA[24]].into_iter())
 }
 
-fn gzip_read_encoder(c: &mut Criterion) {
-    for (i, data) in get_data().into_iter().enumerate() {
+fn read_encoder(c: &mut Criterion) {
+    for (i, data) in get_data().into_iter() {
         let name = format!("igzip::read::GzipEncoder ({})", i);
         c.bench_function(&name, |b| {
             b.iter(|| {
@@ -60,39 +66,35 @@ fn gzip_read_encoder(c: &mut Criterion) {
     }
 }
 
-fn gzip_write_encoder(c: &mut Criterion) {
-    let datas = get_data();
-    c.bench_function("igzip::write::GzipEncoder", |b| {
-        b.iter(|| {
-            for data in datas {
+fn write_encoder(c: &mut Criterion) {
+    for (i, data) in get_data() {
+        let name = format!("igzip::write::GzipEncoder ({})", i);
+        c.bench_function(&name, |b| {
+            b.iter(|| {
                 let mut output = vec![];
                 let mut encoder =
                     igzip::write::GzipEncoder::new(&mut output, igzip::CompressionLevel::Three);
                 let _ = io::copy(&mut Cursor::new(&data), &mut encoder).unwrap();
-            }
-        })
-    });
-}
-fn flate2_gzip_write_encoder(c: &mut Criterion) {
-    let datas = get_data();
-    c.bench_function("flate2::write::GzEncoder", |b| {
-        b.iter(|| {
-            for data in datas {
+            })
+        });
+
+        let name = format!("flate2::write::GzEncoder ({})", i);
+        c.bench_function(&name, |b| {
+            b.iter(|| {
                 let mut output = vec![];
                 let mut encoder =
                     flate2::write::GzEncoder::new(&mut output, flate2::Compression::best());
                 let _ = io::copy(&mut Cursor::new(&data), &mut encoder).unwrap();
-            }
-        })
-    });
+            })
+        });
+    }
 }
 
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(50);
     targets =
-        gzip_read_encoder,
-        gzip_write_encoder,
-        flate2_gzip_write_encoder
+        read_encoder,
+        write_encoder,
 }
 criterion_main!(benches);
